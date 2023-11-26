75eb49faa3e0e4df911cb108758f35c217221774
Matheus Natividade <mathnatividade@gmail.com>
2023-11-26 11:23:55 -0300

    Script for create HTML pages based on JSON file

---
+++ makepages.py
@@ -0,0 +1,23 @@
+import json
+from jinja2 import Template
+
+# Carrega dados do JSON
+with open('resources/posts.json') as json_file:
+    dados = json.load(json_file)
+
+# Carrega o template HTML
+with open('templates/model.html') as template_file:
+    template = Template(template_file.read())
+
+# Gera páginas HTML
+for postagem in dados:
+    html = template.render(
+        titulo=postagem['titulo'],
+        autor=postagem['autor'],
+        data=postagem['data'],
+        conteudo=postagem['conteudo']
+    )
+
+    # Salva o HTML gerado em um arquivo
+    with open(f'posts/{postagem["titulo"].lower().replace(" ", "_")}.html', 'w') as output_file:
+        output_file.write(html)
